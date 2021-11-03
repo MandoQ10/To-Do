9af5af6b3f01833839453de4f264b26bb32233e9
MandoQ10 <aquintana@8thlight.com>
2021-11-03 09:35:32 -0500

    Added Gems for Heroku and home_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'home_page#index'
+  # get "/home_page", to: "home_page#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
