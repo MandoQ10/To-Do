7c23fd2d3bff60fc6fc9227f759ca7a5335d007f
MandoQ10 <aquintana@8thlight.com>
2021-11-22 11:51:59 -0600

    Updated Dependency list and db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_210649) do
+ActiveRecord::Schema.define(version: 2021_11_19_160201) do
 
   create_table "to_do_entries", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "completed"
+    t.date "due_date"
   end
 
 end
