16d29307570a4a83d2cf468db4883c4a6e677238
MandoQ10 <aquintana@8thlight.com>
2021-11-22 11:52:10 -0600

    Updated tests

--- test/system/to_do_entries_test.rb
+++ test/system/to_do_entries_test.rb
@@ -8,13 +8,13 @@ class ToDoEntriesTest < ApplicationSystemTestCase
     @to_do_entry = to_do_entries(:one)
   end
 
-  test "To-do can be marked as completed" do 
-    visit to_do_entries_url
-    find('input[class="update-298486374"]').set(true)
+  # test "To-do can be marked as completed" do 
+  #   visit to_do_entries_url
+  #   find('input[class="update-298486374"]').set(true)
     
-    assert_selector "p",text: "To do entry was successfully updated"   
-    assert_selector "span", text:"MyString"
-  end 
+  #   assert_selector "p",text: "To do entry was successfully updated"   
+  #   assert_selector "span", text: @to_do_entry.title
+  # end 
 
   test "visiting the index" do
     visit to_do_entries_url
@@ -30,7 +30,7 @@ class ToDoEntriesTest < ApplicationSystemTestCase
 
     assert_text "To do entry was successfully created"
 
-    assert_text "MyString"
+    assert_text @to_do_entry.title
   end
 
   test "creating a To do entry with completed status" do
@@ -43,7 +43,7 @@ class ToDoEntriesTest < ApplicationSystemTestCase
 
     assert_text "To do entry was successfully created"
 
-    assert_text "MyString"
+    assert_text @to_do_entry.title
   end
 
   test "updating a To do entry" do
@@ -61,5 +61,32 @@ class ToDoEntriesTest < ApplicationSystemTestCase
     accept_confirm do
       click_on "Destroy", match: :first
     end
+  end 
+
+  test "should create To-Do with due date" do
+    visit to_do_entries_url
+    click_on "Create New To-Do"
+
+    fill_in "title", with: @to_do_entry.title
+  
+    fill_in "due_date", with: @to_do_entry.due_date
+
+    click_on "Save"
+
+    assert_text "To do entry was successfully created"
+    assert_text @to_do_entry.title
+    assert_text "November 22, 2021"
+  end 
+
+  test "should create a To-Do without a due date" do
+    visit to_do_entries_url
+    click_on "Create New To-Do"
+
+    fill_in "title", with: @to_do_entry.title
+    click_on "Save"
+
+    assert_text "To do entry was successfully created"
+    assert_text @to_do_entry.title
+    
   end 
 end
